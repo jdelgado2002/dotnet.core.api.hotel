6a81123a345e4e5c7fd0d52d7fada5cb9abe5df1
Jose M. Delgado <jose.delgado@rackspace.com>
2018-10-25 15:15:26 -0500

    adding root get method

---
+++ Hotel/Hotel/Controllers/RootController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
