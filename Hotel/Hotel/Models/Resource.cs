3a2dc9358581be4f887d84a3fcdd67e28f40eba8
Jose M. Delgado <jose.delgado@rackspace.com>
2018-11-08 15:10:38 -0600

    Serialize exception as Json, creating resource class

---
+++ Hotel/Hotel/Models/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Hotel.Models
+{
+    public abstract class Resource
+    {
+        [JsonProperty(Order = -2)]
+        public string Href { get; set; }
+    }
+}
